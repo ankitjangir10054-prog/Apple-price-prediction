583bc72c26f5ada0a8fb2cf30208f346b305c80b
Ankit Kumar <ankitjangir10054@gmail.com>
2026-07-15 10:28:17 +0530

    Update PredictionForm.jsx

--- src/components/PredictionForm.jsx
+++ src/components/PredictionForm.jsx
@@ -81,7 +81,7 @@ function PredictionForm({ onPredict }) {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://apple-price-prediction.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
