327b32aa11eb1f8d490cc7d0bfd5df76e22b91fe
Ankit Kumar <ankitjangir10054@gmail.com>
2026-07-15 10:36:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Apple Device Price Prediction API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://apple-price-prediction.onrender.com"],
+                   "https://apple-price-prediction-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
